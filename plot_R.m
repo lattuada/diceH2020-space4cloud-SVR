9f38f5d917f215e758332e95b852c56d5a26aa92
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2015-10-21 12:54:50 +0200

    Add function to plot response time

---
+++ plot_R.m
@@ -0,0 +1,21 @@
+## -*- texinfo -*- 
+## @deftypefn {Function File} {@var{h} =} plot_R (@var{y}, @var{X})
+##
+## Plot response time, @var{y}, against number of cores and dataset size found
+## in @var{X}.
+## Return a handle @var{h} to the plot.
+##
+## @end deftypefn
+
+function [h] = plot_R (y, X)
+
+nCores = X(:, end-1);
+dataSize = X(:, end);
+h = figure;
+plot3 (nCores, dataSize, y, "bx");
+xlabel ('Number of cores');
+ylabel ('Dataset size');
+zlabel ('Response time');
+title ('Job response time against cores and dataset size');
+
+endfunction
