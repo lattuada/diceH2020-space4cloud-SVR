7a59b838417eab9ac331b804714be5e965b0f970
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2015-11-04 18:00:22 +0100

    Add model_selection function

--- compare_accuracy.m
+++ compare_accuracy.m
@@ -15,95 +15,32 @@ sample_nCores(:, end) = 1 ./ sample_nCores(:, end);
   split_sample (values, X_nCores, 0.6, 0.2);
 
 MSEs = zeros (4, 1);
+range = [0.1 0.5];
 
 %% White box model, nCores
-C = Inf;
-eps = Inf;
-MSE = Inf;
-for (cc = logspace (-5, 5))
-  for (ee = logspace (-5, 5))
-    options = ["-s 3 -t 0 -q -p ", num2str(ee), " -c ", num2str(cc)];
-    model = svmtrain (ytr, Xtr, options);
-    [~, accuracy, ~] = svmpredict (ytst, Xtst, model, "-q");
-    mse = accuracy(2);
-    if (mse < MSE)
-      C = cc;
-      eps = ee;
-      MSE = mse;
-    endif
-  endfor
-endfor
-
-options = ["-s 3 -t 0 -p ", num2str(eps), " -c ", num2str(C)];
+[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 0 -q -h 0", range, range);
+options = ["-s 3 -t 0 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr, Xtr, options);
 [~, accuracy, ~] = svmpredict (ycv, Xcv, model);
 MSEs(1) = accuracy(2);
 
 %% White box model, nCores^(-1)
-C = Inf;
-eps = Inf;
-MSE = Inf;
-for (cc = logspace (-5, 5))
-  for (ee = logspace (-5, 5))
-    options = ["-s 3 -t 0 -q -p ", num2str(ee), " -c ", num2str(cc)];
-    model = svmtrain (ytr_nCores, Xtr_nCores, options);
-    [~, accuracy, ~] = svmpredict (ytst_nCores, Xtst_nCores, model, "-q");
-    mse = accuracy(2);
-    if (mse < MSE)
-      C = cc;
-      eps = ee;
-      MSE = mse;
-    endif
-  endfor
-endfor
-
-options = ["-s 3 -t 0 -p ", num2str(eps), " -c ", num2str(C)];
+[C, eps] = model_selection (ytr_nCores, Xtr_nCores, ytst_nCores, Xtst_nCores, "-s 3 -t 0 -q -h 0", range, range);
+options = ["-s 3 -t 0 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr_nCores, Xtr_nCores, options);
 [~, accuracy, ~] = svmpredict (ycv_nCores, Xcv_nCores, model);
 MSEs(2) = accuracy(2);
 
 %% Black box model, Polynomial
-C = Inf;
-eps = Inf;
-MSE = Inf;
-for (cc = logspace (-5, 5))
-  for (ee = logspace (-5, 5))
-    options = ["-s 3 -t 1 -q -p ", num2str(ee), " -c ", num2str(cc)];
-    model = svmtrain (ytr, Xtr, options);
-    [~, accuracy, ~] = svmpredict (ytst, Xtst, model, "-q");
-    mse = accuracy(2);
-    if (mse < MSE)
-      C = cc;
-      eps = ee;
-      MSE = mse;
-    endif
-  endfor
-endfor
-
-options = ["-s 3 -t 1 -p ", num2str(eps), " -c ", num2str(C)];
+[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 1 -q -h 0", range, range);
+options = ["-s 3 -t 1 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr, Xtr, options);
 [~, accuracy, ~] = svmpredict (ycv, Xcv, model);
 MSEs(3) = accuracy(2);
 
 %% Black box model, RBF
-C = Inf;
-eps = Inf;
-MSE = Inf;
-for (cc = logspace (-5, 5))
-  for (ee = logspace (-5, 5))
-    options = ["-s 3 -t 2 -q -p ", num2str(ee), " -c ", num2str(cc)];
-    model = svmtrain (ytr, Xtr, options);
-    [~, accuracy, ~] = svmpredict (ytst, Xtst, model, "-q");
-    mse = accuracy(2);
-    if (mse < MSE)
-      C = cc;
-      eps = ee;
-      MSE = mse;
-    endif
-  endfor
-endfor
-
-options = ["-s 3 -t 2 -p ", num2str(eps), " -c ", num2str(C)];
+[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 2 -q -h 0", range, range);
+options = ["-s 3 -t 2 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr, Xtr, options);
 [~, accuracy, ~] = svmpredict (ycv, Xcv, model);
 MSEs(4) = accuracy(2);
