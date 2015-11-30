e35633e50d99b3e0f0698e7cf991a7995c0316b5
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2015-11-30 17:51:20 +0100

    Memorize Cs and epsilons

--- compare_accuracy.m
+++ compare_accuracy.m
@@ -15,38 +15,49 @@ sample_nCores(:, end) = 1 ./ sample_nCores(:, end);
   split_sample (values, X_nCores, 0.6, 0.2);
 
 RMSEs = zeros (4, 1);
-range = [0.1 0.5];
+Cs = zeros (4, 1);
+Es = zeros (4, 1);
+C_range = [0.1 0.5];
+E_range = [0.1 0.5];
 
 %% White box model, nCores
-[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 0 -q -h 0", range, range);
+[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 0 -q -h 0", C_range, E_range);
 options = ["-s 3 -t 0 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr, Xtr, options);
 [~, accuracy, ~] = svmpredict (ycv, Xcv, model);
+Cs(1) = C;
+Es(1) = eps;
 RMSEs(1) = sqrt (accuracy(2));
 
 %% White box model, nCores^(-1)
-[C, eps] = model_selection (ytr_nCores, Xtr_nCores, ytst_nCores, Xtst_nCores, "-s 3 -t 0 -q -h 0", range, range);
+[C, eps] = model_selection (ytr_nCores, Xtr_nCores, ytst_nCores, Xtst_nCores, "-s 3 -t 0 -q -h 0", C_range, E_range);
 options = ["-s 3 -t 0 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr_nCores, Xtr_nCores, options);
 [~, accuracy, ~] = svmpredict (ycv_nCores, Xcv_nCores, model);
+Cs(2) = C;
+Es(2) = eps;
 RMSEs(2) = sqrt (accuracy(2));
 
 %% Black box model, Polynomial
-[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 1 -q -h 0", range, range);
+[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 1 -q -h 0", C_range, E_range);
 options = ["-s 3 -t 1 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr, Xtr, options);
 [~, accuracy, ~] = svmpredict (ycv, Xcv, model);
+Cs(3) = C;
+Es(3) = eps;
 RMSEs(3) = sqrt (accuracy(2));
 
 %% Black box model, RBF
-[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 2 -q -h 0", range, range);
+[C, eps] = model_selection (ytr, Xtr, ytst, Xtst, "-s 3 -t 2 -q -h 0", C_range, E_range);
 options = ["-s 3 -t 2 -h 0 -p ", num2str(eps), " -c ", num2str(C)];
 model = svmtrain (ytr, Xtr, options);
 [~, accuracy, ~] = svmpredict (ycv, Xcv, model);
+Cs(4) = C;
+Es(4) = eps;
 RMSEs(4) = sqrt (accuracy(2));
 
 percent_RMSEs = RMSEs / max (RMSEs);
-rel_RMSEs = RMSEs / mean (values);
+rel_RMSEs = RMSEs / median (values);
 
 RMSEs
 percent_RMSEs
