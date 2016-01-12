8238f4541c10739142cecb6fbc2c6d953d66bf15
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-01-12 17:15:02 +0100

    Fix plots in small dimensions

--- compare_accuracy_big.m
+++ compare_accuracy_big.m
@@ -2,9 +2,15 @@ clear all
 close all hidden
 clc
 
-query = "R1";
+%% Parameters
+query = "R1_two_cols";
 base_dir = "/home/eugenio/Desktop/cineca-runs-20150111/";
 
+dimensions = 2;
+C_range = linspace (0.1, 5, 20);
+E_range = linspace (0.1, 5, 20);
+
+%% Real stuff
 [values, sample] = read_from_directory ([base_dir, query, "/small"]);
 [big_values, big_sample] = read_from_directory ([base_dir, query, "/big"]);
 
@@ -47,10 +53,6 @@ ycv = big_y;
 Xcv = big_X;
 ycv_nCores = big_y_nCores;
 Xcv_nCores = big_X_nCores;
-
-small_dimensional = false;
-C_range = linspace (0.1, 5, 20);
-E_range = linspace (0.1, 5, 20);
 
 RMSEs = zeros (1, 4);
 Cs = zeros (1, 4);
@@ -116,6 +118,63 @@ mean_predictions = mean (predictions);
 err_mean = mean_predictions - mean_y;
 rel_err_mean = err_mean / mean_y;
 
+%% Plots
+switch (dimensions)
+  case {1}
+    figure;
+    plot (X, y, "g+");
+    hold on;
+    func = @(x) w{1} .* x + b{1};
+    extremes = xlim ();
+    x = linspace (extremes(1), extremes(2), 10);
+    plot (x, func (x), "r-");
+    axis auto;
+    title ("Linear kernels");
+    grid on;
+    
+    figure;
+    plot (X_nCores, y_nCores, "g+");
+    hold on;
+    func = @(x) w{2} .* x + b{2};
+    extremes = xlim ();
+    x = linspace (extremes(1), extremes(2), 10);
+    plot (x, func (x), "r-");
+    axis auto;
+    title ('Linear kernels, nCores^{- 1}');
+    grid on;
+  case {2}
+    figure;
+    plot3 (X(:, 1), X(:, 2), y, "g+");
+    hold on;
+    func = @(x, y) w{1}(1) .* x + w{1}(2) .* y + b{1};
+    extremes = xlim ();
+    x = linspace (extremes(1), extremes(2), 10);
+    extremes = xlim ();
+    x = linspace (extremes(1), extremes(2), 10);
+    extremes = ylim ();
+    yy = linspace (extremes(1), extremes(2), 10);
+    [XX, YY] = meshgrid (x, yy);
+    surf (XX, YY, func (XX, YY));
+    axis auto;
+    title ("Linear kernels");
+    grid on;
+    
+    figure;
+    plot3 (X_nCores(:, 1), X_nCores(:, 2), y_nCores, "g+");
+    hold on;
+    func = @(x, y) w{2}(1) .* x + w{2}(2) .* y + b{2};
+    extremes = xlim ();
+    x = linspace (extremes(1), extremes(2), 10);
+    extremes = ylim ();
+    yy = linspace (extremes(1), extremes(2), 10);
+    [XX, YY] = meshgrid (x, yy);
+    surf (XX, YY, func (XX, YY));
+    axis auto;
+    title ('Linear kernels, nCores^{- 1}');
+    grid on;
+endswitch
+
+%% Print metrics
 RMSEs
 percent_RMSEs
 rel_RMSEs
@@ -124,23 +183,3 @@ min_rel_err
 mean_abs_err
 rel_mean_abs_err
 rel_err_mean
-
-if (small_dimensional)
-  figure;
-  h = plot (X, y, "g+");
-  hold on;
-  func = @(x) w{1}' * x + b{1};
-  ezplot (func, get (h, "xlim"));
-  axis auto;
-  title ("Linear kernels");
-  grid on;
-
-  figure;
-  h = plot (X_nCores, y_nCores, "g+");
-  hold on;
-  func = @(x) w{2}' * x + b{2};
-  ezplot (func, get (h, "xlim"));
-  axis auto;
-  title ('Linear kernels, nCores^{- 1}');
-  grid on;
-endif
