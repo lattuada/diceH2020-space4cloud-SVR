e1091e8c722e22d1a74b0feddc501f8c2a76e461
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-01-19 14:13:26 +0100

    Interpolate when printing learning curves

--- print_learning_curve.m
+++ print_learning_curve.m
@@ -2,23 +2,28 @@ clear all
 close all hidden
 clc
 
-train_frac = 0.8;
+%% Parameters
+query = "R1_two_cols";
+
+train_frac = 0.6;
 test_frac = 0.2;
 
-sample = read_from_directory ("/home/eugenio/Desktop/cineca-runs-20150111/R1/small");
-sample_big = read_from_directory ("/home/eugenio/Desktop/cineca-runs-20150111/R1/big");
+%% Work
+base_dir = "/home/eugenio/Desktop/cineca-runs-20150111/";
+sample = read_from_directory ([base_dir, query, "/small"]);
+sample_big = read_from_directory ([base_dir, query, "/big"]);
 
-scaled = zscore (sample_big);
-ycv = scaled(:, 1);
-Xcv = scaled(:, 2:end);
-
-scaled = zscore (sample);
+rand ("seed", 17);
+complete_sample = [sample; sample_big];
+scaled = zscore (complete_sample);
 scaled = scaled(randperm (size (scaled, 1)), :);
-[train, test, ~] = split_sample (scaled, train_frac, test_frac);
+[train, test, cv] = split_sample (scaled, train_frac, test_frac);
 ytr = train(:, 1);
 Xtr = train(:, 2:end);
 ytst = test(:, 1);
 Xtst = test(:, 2:end);
+ycv = cv(:, 1);
+Xcv = cv(:, 2:end);
 
 C_range = linspace (0.1, 5, 20);
 E_range = linspace (0.1, 5, 20);
